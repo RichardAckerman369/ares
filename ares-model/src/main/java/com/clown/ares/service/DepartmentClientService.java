4ea33941f254c811b640ff8d390c913914f82e77
Richard <ljy8953898@gmail.com>
2019-01-26 15:11:46 +0800

    添加feign模块

---
+++ ares-model/src/main/java/com/clown/ares/service/DepartmentClientService.java
@@ -0,0 +1,29 @@
+package com.clown.ares.service;
+
+import com.clown.ares.entity.Department;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(value = "ARES-PROVIDER")
+public interface DepartmentClientService {
+
+    @GetMapping("/department/findAll")
+    List<Department> findAll();
+
+    @GetMapping("/department/findById/{departmentId}")
+    Department findById(@PathVariable("departmentId") Long departmentId);
+
+    @PostMapping("/department/add")
+    String add(@RequestBody Department department);
+
+    @PutMapping("/department/update")
+    String update(@RequestBody Department department);
+
+    @DeleteMapping("/department/delete/{departmentId}")
+    String delete(@PathVariable("departmentId") Long departmentId);
+
+    @GetMapping("/department/discovery")
+    Object discovery();
+}
